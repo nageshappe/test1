c6b652d4d2bf2f9af36a0a0f130120750ec44d5c
nageshappe <nageshf25@gmail.com>
2019-03-22 06:44:22 +0530

    Updates

---
+++ precache-manifest.e90a7970ae8b999ac2c61f5d2767e82b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9952dc3f0fc82d3c18d0",
+    "url": "/helloworld/static/js/runtime~main.c32053ff.js"
+  },
+  {
+    "revision": "8bdbcfd8ccf62ff8eae8",
+    "url": "/helloworld/static/js/main.c7e8bdf2.chunk.js"
+  },
+  {
+    "revision": "d14408cb011c7113242e",
+    "url": "/helloworld/static/js/2.4b8ce697.chunk.js"
+  },
+  {
+    "revision": "8bdbcfd8ccf62ff8eae8",
+    "url": "/helloworld/static/css/main.af73ed7f.chunk.css"
+  },
+  {
+    "revision": "5a2a057d997ed89e923f693f107c5b77",
+    "url": "/helloworld/index.html"
+  }
+];
